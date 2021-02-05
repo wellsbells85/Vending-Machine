15a54562b94d5d4fd9bf47362530f66691bc06fb
Nicholas Weller <nicholasbruceweller@gmail.com>
2021-02-05 16:19:29 -0500

    Working on getting AuditWriter functioning.

--- src/main/java/com/techelevator/AuditWriter.java
+++ src/main/java/com/techelevator/AuditWriter.java
@@ -1,10 +1,39 @@
 package com.techelevator;
 
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.time.LocalDateTime;    
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class AuditWriter {
 	
-	//every time there is any money transaction log it
-	//these come from Accounting.java, SelectProduct.java, and FeedMoneyMenu.java
-	//will to date/time stamp log
-	//will to read/write IO 'Log.txt'
+	String pathName = "FizzBuzz.txt"; 
+	File fileName = new File(pathName); 
 	
+	if(!fileName.exists() ) {
+		try {
+			fileName.createNewFile(); //write the file
+		} catch (IOException e) {		
+			System.exit(1); //end with an irregular error
+		} // end try-catch
+	}	
+//	
+//		
+//	
+//
+//	try (PrintWriter writer = new PrintWriter(new FileWriter(fileName, false ))) {
+//		for(int i = 1; i <= 300; i++) {
+//			String textToPrint = FizzBuzz(i);
+//			writer.println(textToPrint);
+//		}	writer.close();		
+//	} catch(IOException e) {
+//		System.out.println("\nThe program was unable to write your file. Sorry.");
+//		System.exit(1); //end the program with an irregular error
+//	}
+
+
 }
+
