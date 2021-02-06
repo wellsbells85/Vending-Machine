8596f2882aa5c97ccaccaa6c3c66629ac03d69d6
Mike Vay <mikevay@gmail.com>
2021-02-05 22:22:25 -0500

    finsished all parts of audit except for wiping file when program closes

--- src/main/java/com/techelevator/AuditWriter.java
+++ src/main/java/com/techelevator/AuditWriter.java
@@ -10,16 +10,26 @@ import java.io.PrintWriter;
 
 public class AuditWriter {
 	
-	 
+	private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a");
+	private LocalDateTime time = LocalDateTime.now();
+	
+	
 	public void logWriter(String message) {
-		try (PrintWriter writer = new PrintWriter(new FileWriter("Log.txt", false ))) {
-			writer.println(message);
+		try (PrintWriter writer = new PrintWriter(new FileWriter("Log.txt", true ))) { // changed this to true so it adds to the list in the file
+			writer.println(dateFormatter.format(time) + " " + message);
 			writer.close();		
 		} catch(IOException e) {
 		System.exit(1); //end the program with an irregular error
 		}
 	}
 	
+	/*
+	 * Feed Money - DONE
+	 * Give Change - DONE
+	 * Purchase - DONE
+	 * 
+	 * We will need a method that will initialize a new empty log file at the start of each run, or we can have a master log file like the Sales Report will.
+	 */
 }	
 	
 	
