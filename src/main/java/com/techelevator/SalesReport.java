13aa67f011cad703e3eeadf35be200968ddb1675
Mike Vay <mikevay@gmail.com>
2021-02-05 22:22:49 -0500

    started sales report

--- src/main/java/com/techelevator/SalesReport.java
+++ src/main/java/com/techelevator/SalesReport.java
@@ -1,5 +1,10 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class SalesReport {
 
 	//create a private map on startup that increments value with every sale  
@@ -15,4 +20,18 @@ public class SalesReport {
 //	>Moonpie|23
 //	>Cowtales|2
 
+	private File salesReport = new File("SalesReport.txt");
+	
+	public void makeReport() throws IOException {
+		salesReport.createNewFile();
+	}
+	
+	public void logWriter(String message) {
+		try (PrintWriter writer = new PrintWriter(new FileWriter(salesReport, true ))) {
+			writer.println(message);
+			writer.close();		
+		} catch(IOException e) {
+		System.exit(1); //end the program with an irregular error
+		}
+	}
 }
